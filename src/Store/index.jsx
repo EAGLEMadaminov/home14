e6be6224ca5b1ed43b1588dda641c7adc63b34ee
EAGLEMadaminjonov <javohirweb99@gamil.com>
2024-01-10 06:54:50 +0500

    add

--- src/Store/index.jsx
+++ src/Store/index.jsx
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import todoReducer from "./Slices/todoSlice";
-import SettingsReducer from "./Slices/SettingsReducer";
+// import SettingsReducer from "./Slices/SettingsReducer";
 
 const store = configureStore({
   reducer: {
     todo: todoReducer,
-    settings: SettingsReducer,
+    // settings: SettingsReducer,
   },
 });
 
